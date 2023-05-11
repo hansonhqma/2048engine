c20fa38de108db86b09d802d48bc59d0cafbe4c2
Hanson Ma <hanson@hansons-mbp.lan>
2023-05-11 00:24:52 -0700

    supports exporting game state as torch tensor

--- game.py
+++ game.py
@@ -13,10 +13,18 @@ class engine:
         # insert two random tiles
         self.__insertrandomtile__()
         self.__insertrandomtile__()
+        
+        try:
+            import torch
+            self.__torch__ = torch
+        except ModuleNotFoundError:
+            self.__torch__ = None
 
-    def move(self, direction):
+    def move(self, direction) -> bool:
         # check move validity
         dir = direction.lower()
+
+        prev_state = self.__STATE__.flatten()
 
         if dir == 'n' or dir == 's':
             self.__STATE__ = self.__STATE__.transpose()
@@ -29,14 +37,26 @@ class engine:
 
         if dir == 'n' or dir == 's':
             self.__STATE__ = self.__STATE__.transpose()
+        
+        if np.array_equal(prev_state, self.__STATE__.flatten()):
+            # invalid move
+            return False
 
         self.__insertrandomtile__()
 
         if np.count_nonzero(self.__STATE__ == 0) == 0:
             self.__GAME_ACTIVE__ = False
+        
+        return True
     
     def score(self):
         return self.__SCORE__
+    
+    def active(self) -> bool:
+        """
+        returns whether or not the game is running or over
+        """
+        return self.__GAME_ACTIVE__
 
     def reset(self):
         self.__init__(_size=self.__SIZE_CONST__)
@@ -52,6 +72,14 @@ class engine:
         copy.__GAME_ACTIVE__ = self.__GAME_ACTIVE__
 
         return copy
+    
+    def get_state_as_tensor(self):
+        if not self.__torch__:
+            print("Torch module not found")
+            return None
+        else:
+            return self.__torch__.from_numpy(self.__STATE__.flatten().copy()).type(self.__torch__.float32)
+
 
     def __str__(self):
         return self.__STATE__.__str__()
